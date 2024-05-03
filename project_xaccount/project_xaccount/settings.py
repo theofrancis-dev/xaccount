75862bbe1c493d73ef607e435324555b0db294c1
Teofilo Francisco <teopinillo@gmail.com>
2024-05-03 06:52:53 -0400

    moved build.sh to root folder

--- project_xaccount/project_xaccount/settings.py
+++ project_xaccount/project_xaccount/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','https://payrollify.onrender.com']
 
 
 # Application definition
